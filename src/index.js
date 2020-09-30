8b518d5e7f4b8a6351a77a85bff87e6ca2339d7a
Nicolay-kr <nicolay.krischenovich@gmail.com>
2020-09-30 12:51:42 +0300

    feat: make min, max, avg fuctions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length && array.length !== 0){
+    let min = array[0];
+    for(let i = 1; i < array.length;i++ ){
+      array[i] < min ? min = array[i] : min ;
+    }
+    return min;
+  }else{return 0}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length && array.length !== 0){
+    let max = array[0];
+    for(let i = 1; i < array.length;i++ ){
+      array[i] > max ? max = array[i] : max ;
+    }
+    return max;
+  }else{return 0}
 }
-
+  
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length && array.length !== 0){
+    let avg = 0;
+    for(let i = 0; i < array.length;i++ ){
+      avg += array[i] ;
+    }
+    return avg/array.length;
+  }else{return 0}
 }
